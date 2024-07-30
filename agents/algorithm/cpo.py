dc64a36eb4943bd6d329cadf8a0912fceba8d23b
Dilshan Rakshitha <dilshanrakshitha2000@gmail.com>
2024-07-30 21:56:44 +0530

    updated cpo

--- agents/algorithm/cpo.py
+++ agents/algorithm/cpo.py
@@ -2,5 +2,203 @@ import torch
 import torch.nn as nn
 
 from agents.algorithm.agent import Agent
-class CPO(Agent):
+from agents.models.actor_critic import ActorCritic
+from utils.onpolicy_buffers import RolloutBuffer
+from utils.logger import LogExperiment
+
+
+class PPO(Agent):
+    def __init__(self, args, env_args, load_model, actor_path, critic_path):
+        super(PPO, self).__init__(args, env_args=env_args)
+        self.args = args
+        self.env_args = env_args
+        self.device = args.device
+        self.completed_interactions = 0
+
+        # training params
+        self.train_v_iters = args.n_vf_epochs
+        self.train_pi_iters = args.n_pi_epochs
+        self.batch_size = args.batch_size
+        self.pi_lr = args.pi_lr
+        self.vf_lr = args.vf_lr
+
+        # load models and setup optimiser.
+        self.policy = ActorCritic(args, load_model, actor_path, critic_path).to(self.device)
+        if args.verbose:
+            print('PolicyNet Params: {}'.format(sum(p.numel() for p in self.policy.Actor.parameters() if p.requires_grad)))
+            print('ValueNet Params: {}'.format(sum(p.numel() for p in self.policy.Critic.parameters() if p.requires_grad)))
+        self.optimizer_Actor = torch.optim.Adam(self.policy.Actor.parameters(), lr=self.pi_lr)
+        self.optimizer_Critic = torch.optim.Adam(self.policy.Critic.parameters(), lr=self.vf_lr)
+        self.value_criterion = nn.MSELoss()
+
+        self.RolloutBuffer = RolloutBuffer(args)
+        self.rollout_buffer = {}
+
+        # ppo params
+        self.grad_clip = args.grad_clip
+        self.entropy_coef = args.entropy_coef
+        self.eps_clip = args.eps_clip
+        self.target_kl = args.target_kl
+        self.d_k = args.d_k
+        self.max_kl = args.max_kl
+        self.constraint = self.rollout_buffer['constraint']
+
+        # logging
+        self.model_logs = torch.zeros(7, device=self.args.device)
+        self.LogExperiment = LogExperiment(args)
+
+    def Fvp_fim(v):
+        # implement later
+        return 0
+    
+    def Fvp_direct(v):
+        # implement later
+        return 0
+
+    def conjugate_gradients(Avp_f, b, nsteps, rdotr_tol=1e-10):
+        x = torch.zeros(b.size(), device=b.device)
+        r = b.clone()
+        p = b.clone()
+        rdotr = torch.dot(r, r)
+        for i in range(nsteps):
+            Avp = Avp_f(p)
+            alpha = rdotr / torch.dot(p, Avp)
+            x += alpha * p
+            r -= alpha * Avp
+            new_rdotr = torch.dot(r, r)
+            betta = new_rdotr / rdotr
+            p = r + betta * p
+            rdotr = new_rdotr
+            if rdotr < rdotr_tol:
+                break
+        return x
+
+    def train_pi(self):
+        print('Running Policy Update...')
+        temp_loss_log = torch.zeros(1, device=self.device)
+        policy_grad, pol_count = torch.zeros(1, device=self.device), torch.zeros(1, device=self.device)
+        continue_pi_training, buffer_len = True, self.rollout_buffer['len']
+        constraint = self.rollout_buffer['constraint']
+        for i in range(self.train_pi_iters):
+            start_idx, n_batch = 0, 0
+            while start_idx < buffer_len:
+                n_batch += 1
+                end_idx = min(start_idx + self.batch_size, buffer_len)
+
+                states_batch = self.rollout_buffer['states'][start_idx:end_idx, :, :]
+                actions_batch = self.rollout_buffer['action'][start_idx:end_idx, :]
+                logprobs_batch = self.rollout_buffer['log_prob_action'][start_idx:end_idx, :]
+                advantages_batch = self.rollout_buffer['advantage'][start_idx:end_idx]
+                advantages_batch = (advantages_batch - advantages_batch.mean()) / (advantages_batch.std() + 1e-5)
+                cost_advantages_batch = self.rollout_buffer['cost_advantage'][start_idx:end_idx]
+                cost_advantages_batch = (cost_advantages_batch - cost_advantages_batch.mean()) / (cost_advantages_batch.std() + 1e-5)
+
+                logprobs_prediction, dist_entropy = self.policy.evaluate_actor(states_batch, actions_batch)
+                ratios = torch.exp(logprobs_prediction - logprobs_batch)
+                ratios = ratios.squeeze()
+                r_theta = ratios * advantages_batch
+                policy_loss = -r_theta.mean() - self.entropy_coef * dist_entropy.mean()
+
+                # early stop: approx kl calculation
+                log_ratio = logprobs_prediction - logprobs_batch
+                approx_kl = torch.mean((torch.exp(log_ratio) - 1) - log_ratio).detach().cpu().numpy()
+                if approx_kl > 1.5 * self.target_kl:
+                    if self.args.verbose:
+                        print('Early stop => Epoch {}, Batch {}, Approximate KL: {}.'.format(i, n_batch, approx_kl))
+                    continue_pi_training = False
+                    break
+
+                if torch.isnan(policy_loss):  # for debugging only!
+                    print('policy loss: {}'.format(policy_loss))
+                    exit()
+
+                temp_loss_log += policy_loss.detach()
+                policy_grad = torch.nn.utils.clip_grad_norm_(self.policy.Actor.parameters(), self.grad_clip)
+                policy_grad_ += policy_grad # used to returing mean_pi_gradient at the end
+                grads = torch.autograd.grad(policy_loss, self.policy.Actor.parameters())
+                loss_grad = torch.cat([grad.view(-1) for grad in grads])
+                # implement gradient normalizing if want here
+
+                # finding the step direction / add direct hessian finding function here later. get the parameter from args
+                Fvp = self.Fvp_fim
+                stepdir = self.conjugate_gradients(Fvp, -loss_grad, 10)
+                # if gradient normalizing, normalize the step dir here
+
+                # findign cost loss
+                c_theta = ratios * cost_advantages_batch
+                cost_loss = -c_theta.mean() - self.entropy_coef * dist_entropy.mean()
+
+                #finding the cost step direction
+                cost_grads = torch.autograd.grad(cost_loss, self.policy.Actor.parameters(), allow_unused=True)
+                cost_loss_grad = torch.cat([grad.view(-1) for grad in cost_grads]).detach() #a
+                cost_loss_grad = cost_loss_grad/torch.norm(cost_loss_grad)
+                cost_stepdir = self.conjugate_gradients(Fvp, -cost_loss_grad, 10)
+
+                # Define q, r, s
+                p = -cost_loss_grad.dot(stepdir) #a^T.H^-1.g
+                q = -loss_grad.dot(stepdir) #g^T.H^-1.g
+                r = loss_grad.dot(cost_stepdir) #g^T.H^-1.a
+                s = -cost_loss_grad.dot(cost_stepdir) #a^T.H^-1.a 
+
+                self.d_k = torch.tensor(self.d_k).to(self.constraint.dtype).to(self.constraint.device)
+                cc = self.constraint - self.d_k
+                lamda = 2*self.max_kl
+
+                #######
+                temp_loss_log += policy_loss.detach()
+                policy_loss.backward()
+                policy_grad += torch.nn.utils.clip_grad_norm_(self.policy.Actor.parameters(), self.grad_clip)  # clip gradients before optimising
+                pol_count += 1
+                self.optimizer_Actor.step()
+                start_idx += self.batch_size
+
+            if not continue_pi_training:
+                break
+        mean_pi_grad = policy_grad_ / pol_count if pol_count != 0 else 0
+        print('The policy loss is: {}'.format(temp_loss_log))
+        return mean_pi_grad, temp_loss_log
+
+    def train_vf(self):
+        print('Running Value Function Update...')
+
+        # variables to be logged for debugging purposes.
+        val_loss_log, value_grad = torch.zeros(1, device=self.device), torch.zeros(1, device=self.device)
+        true_var, explained_var = torch.zeros(1, device=self.device), torch.zeros(1, device=self.device)
+        val_count = torch.zeros(1, device=self.device)
+
+        for i in range(self.train_v_iters):
+            start_idx = 0
+            while start_idx < self.rollout_buffer['len']:
+                end_idx = min(start_idx + self.batch_size, self.rollout_buffer['len'])
+
+                states_batch = self.rollout_buffer['states'][start_idx:end_idx, :, :]
+                value_target = self.rollout_buffer['value_target'][start_idx:end_idx]
+
+                self.optimizer_Critic.zero_grad()
+                value_prediction = self.policy.evaluate_critic(states_batch)
+                value_loss = self.value_criterion(value_prediction, value_target)
+                value_loss.backward()
+                value_grad += torch.nn.utils.clip_grad_norm_(self.policy.Critic.parameters(), self.grad_clip)  # clip gradients before optimising
+                self.optimizer_Critic.step()
+                val_count += 1
+                start_idx += self.batch_size
+
+                # logging.
+                val_loss_log += value_loss.detach()
+                y_pred = value_prediction.detach().flatten()
+                y_true = value_target.flatten()
+                var_y = torch.var(y_true)
+                true_var += var_y
+                explained_var += 1 - torch.var(y_true - y_pred) / (var_y + 1e-5)
+
+        return value_grad / val_count, val_loss_log, explained_var / val_count, true_var / val_count
+
+    def update(self):
+        self.rollout_buffer = self.RolloutBuffer.prepare_rollout_buffer()
+        self.model_logs[0], self.model_logs[5] = self.train_pi()
+        self.model_logs[1], self.model_logs[2], self.model_logs[3], self.model_logs[4] = self.train_vf()
+        self.LogExperiment.save(log_name='/model_log', data=[self.model_logs.detach().cpu().flatten().numpy()])
+
+    
+
 
--- utils/onpolicy_buffers.py
+++ utils/onpolicy_buffers.py
@@ -9,6 +9,7 @@ class RolloutBuffer:
     def __init__(self, args):
         self.size = args.n_step
         self.device = args.device
+        self.constraint_value = 0.0
 
         # discounted vs average reward RL
         self.return_type = args.return_type
@@ -35,6 +36,8 @@ class RolloutBuffer:
         self.reward = torch.rand(self.n_training_workers, self.n_step, device=self.device)
         self.v_targ = torch.rand(self.n_training_workers, self.n_step, device=self.device)
         self.adv = torch.rand(self.n_training_workers, self.n_step, device=self.device)
+        self.cost = torch.rand(self.n_training_workers, self.n_step, device=self.device)
+        self.cadv = torch.rand(self.n_training_workers, self.n_step, device=self.device)
         self.v_pred = torch.rand(self.n_training_workers, self.n_step + 1, device=self.device)
         self.first_flag = torch.rand(self.n_training_workers, self.n_step + 1, device=self.device)
 
@@ -46,6 +49,7 @@ class RolloutBuffer:
         self.v_pred[training_agent_index] = data['v_pred']
         self.reward[training_agent_index] = data['reward']
         self.first_flag[training_agent_index] = data['first_flag']
+        self.cost[training_agent_index] = data['cost']
 
     def compute_gae(self):  # TODO: move to different script, optimise/resolve moving across devices back and forth.
         orig_device = self.v_pred.device
@@ -65,6 +69,41 @@ class RolloutBuffer:
             adv[:, t] = lastgaelam = delta + notlast * self.gamma * self.lambda_ * lastgaelam
         vtarg = vpred[:, :-1] + adv
         return adv.to(device=orig_device), vtarg.to(device=orig_device)
+    
+    def computer_gcae(self):
+        orig_device = self.v_pred.device
+        assert orig_device == self.cost.device == self.first_flag.device
+        vpred, cost, first = (x.cpu() for x in (self.v_pred, self.cost, self.first_flag))
+        first = first.to(dtype=torch.float32)
+        assert first.dim() == 2
+        nenv, nsteps = cost.shape
+        assert vpred.shape == first.shape == (nenv, nsteps + 1)
+        cadv = torch.zeros(nenv, nsteps, dtype=torch.float32)
+        lastcgaelam = 0
+        for t in reversed(range(nsteps)):
+            notlast = 1.0 - first[:, t+1]
+            nextvalue = vpred[:, t+1]
+            delta = cost[:, t] + notlast*self.gamma*nextvalue - vpred[:, t]
+            cadv[:, t] = lastcgaelam = delta + notlast* self.gamma * self.lambda_ *lastcgaelam
+        return cadv.to(device=orig_device)
+    
+    # define the compute cost function
+    def estimate_constraint_value(self):
+        orig_device = self.cost.device
+        assert orig_device == self.first_flag.device
+        first, cost = (x.cpu() for x in (self.first_flag, self.cost))
+        first = first.to(dtype=torch.float32)
+        assert first.dim() == 2
+        nenv, nstep = cost.shape
+        assert first.shape == (nenv, nstep+1)
+        constraint_value_ = torch.tensor(nenv)
+        for i in range(nstep-1):
+            constraint_value_ = constraint_value_ + self.costs[:,i] * self.gamma**(i)
+        
+        constraint_value = torch.sum(constraint_value_)
+        constraint_value = constraint_value/nenv
+        constraint_value = constraint_value.to(device = orig_device)
+        return constraint_value
 
     def prepare_rollout_buffer(self):
 
@@ -72,11 +111,15 @@ class RolloutBuffer:
             if self.normalize_reward:  # reward normalisation
                 self.reward = self.reward_normaliser(self.reward, self.first_flag)
             self.adv, self.v_targ = self.compute_gae()  # # calc returns
+            self.cadv = self.computer_gcae()
 
         if self.return_type == 'average':
             self.reward = self.reward_normaliser(self.reward, self.first_flag, type='average')
             self.adv, self.v_targ = self.compute_gae()
+            self.cadv = self.computer_gcae()
 
+        #get the constraint value
+        self.constraint_value = self.estimate_constraint_value()
 
         '''concat data from different workers'''
         s_hist = self.old_states.view(-1, self.feature_history, self.n_features)
@@ -84,6 +127,7 @@ class RolloutBuffer:
         logp = self.old_logprobs.view(-1, 1)
         v_targ = self.v_targ.view(-1)
         adv = self.adv.view(-1)
+        cadv = self.cadv.view(-1)
         first_flag = self.first_flag.view(-1)
         buffer_len = s_hist.shape[0]
 
@@ -94,8 +138,9 @@ class RolloutBuffer:
             logp = logp[rand_perm, :]  # torch.Size([batch, 1])
             v_targ = v_targ[rand_perm]  # torch.Size([batch])
             adv = adv[rand_perm]  # torch.Size([batch])
+            cadv = cadv[rand_perm]
 
-        self.rollout_buffer = dict(states=s_hist, action=act, log_prob_action=logp, value_target=v_targ, advantage=adv, len=buffer_len)
+        self.rollout_buffer = dict(states=s_hist, action=act, log_prob_action=logp, value_target=v_targ, advantage=adv, len=buffer_len, cost_advantage = cadv, constraint = self.constraint_value)
         return self.rollout_buffer
 
 
@@ -111,6 +156,7 @@ class RolloutWorker:
         self.state = np.zeros(core.combined_shape(self.size, (self.feature_hist, self.features)), dtype=np.float32)
         self.actions = np.zeros(self.size, dtype=np.float32)
         self.rewards = np.zeros(self.size, dtype=np.float32)
+        self.cost = np.zeros(self.size, dtype=np.float32)
         self.state_values = np.zeros(self.size + 1, dtype=np.float32)
         self.logprobs = np.zeros(self.size, dtype=np.float32)
         self.first_flag = np.zeros(self.size + 1, dtype=np.bool_)
@@ -137,6 +183,6 @@ class RolloutWorker:
         assert self.ptr == self.max_size
         self.ptr, self.path_start_idx = 0, 0
         data = dict(obs=self.state, act=self.actions, v_pred=self.state_values,
-                    logp=self.logprobs, first_flag=self.first_flag, reward=self.rewards, cgm_target=self.cgm_target)
+                    logp=self.logprobs, first_flag=self.first_flag, reward=self.rewards, cost = self.cost, cgm_target=self.cgm_target)
         return {k: torch.as_tensor(v, dtype=torch.float32, device=self.device) for k, v in data.items()}
 
