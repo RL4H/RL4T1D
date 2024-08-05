30146697184b30ae29d7fb5c7f1e217934ff92cc
dilshanrakshitha <dilshanrakshitha2000@gmail.com>
2024-08-05 13:16:50 +0000

    fixing bugs

--- agents/models/policy.py
+++ agents/models/policy.py
@@ -28,13 +28,14 @@ class PolicyModule(nn.Module):
         fc_output1 = F.relu(self.fc_layer1(extract_states))
         fc_output2 = F.relu(self.fc_layer2(fc_output1))
         fc_output = F.relu(self.fc_layer3(fc_output2))
-        # removed normalization of expected vaalue
-        mu = F.tanh(fc_output)
+        # removed normalization of expected vaalue. Although the output is sending through the normalization, see inside normalization block. it is not done.
+        # print("fc_output\n")
+        # print(fc_output)
+        mu = F.tanh(self.mu(fc_output))
         sigma = F.sigmoid(self.sigma(fc_output) + 1e-5)
         z = self.normalDistribution(0, 1).sample()
         action = mu + sigma * z
         action = torch.clamp(action, -1, 1)
-        # print('\nCurrent mu: {}, sigma: {}'.format(mu, sigma))
         try:
             dst = self.normalDistribution(mu, sigma)
             log_prob = dst.log_prob(action[0])
