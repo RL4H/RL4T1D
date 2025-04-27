602842c2569ffff95f4da1151a185a911bdf7339
Jordan Triming <u7116534@anu.edu.au>
2025-04-27 18:28:03 +1000

    scripts

---
+++ experiments/nci/old/irl_proj0.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#PBS -P sj53
+#PBS -q gpuvolta
+#PBS -l walltime=24:00:00
+#PBS -l mem=24GB
+#PBS -l jobfs=0
+#PBS -l ngpus=1
+#PBS -l ncpus=12
+#PBS -M u7116534@anu.edu.au
+#PBS -l storage=scratch/sj53
+#PBS -o out_proj0.txt
+#PBS -e err_proj0.txt
+#PBS -l software=python
+
+module load pytorch/1.9.0
+cd /scratch/sj53/jt3998/RL4T1D/
+
+python3 experiments/run_irl.py --patient_id 0 --n_expert 100 --l_expert 256 --i_irl 50 --i_update_init 16 --i_update 4 --n_sim 2 --l_sim 256
+wait
