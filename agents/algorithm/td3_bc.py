fixed control space scaling

agents/algorithm/td3_bc.py
@@ -16,7 +16,7 @@ from collections import namedtuple, deque
 MAIN_PATH = config('MAIN_PATH')
 sys.path.insert(1, MAIN_PATH)
 
-Transition = namedtuple('Transition', ('state', 'feat', 'action', 'reward', 'next_state', 'next_feat', 'done'))
+Transition = namedtuple('Transition', ('state', 'action', 'reward', 'next_state', 'done'))
 
 
 DEFAULT_FEAT = 0
@@ -114,21 +114,19 @@ class TD3_BC(Agent):
 
             batch = Transition(*zip(*transitions))
             cur_state_batch = torch.cat(batch.state)
-            cur_feat_batch = torch.cat(batch.feat)
             actions_batch = torch.cat(batch.action)
             reward_batch = torch.cat(batch.reward).unsqueeze(1)
             next_state_batch = torch.cat(batch.next_state)
-            next_feat_batch = torch.cat(batch.next_feat)
             done_batch = torch.cat(batch.done).unsqueeze(1)
 
             # value network update
-            new_action, next_log_prob = self.policy.evaluate_target_policy_noise(next_state_batch, next_feat_batch)
-            next_values = torch.min(self.policy.value_net_target1(next_state_batch, next_feat_batch, new_action),
-                                    self.policy.value_net_target2(next_state_batch, next_feat_batch, new_action))
+            new_action, next_log_prob = self.policy.evaluate_target_policy_noise(next_state_batch)
+            next_values = torch.min(self.policy.value_net_target1(next_state_batch, new_action),
+                                    self.policy.value_net_target2(next_state_batch, new_action))
             target_value = (reward_batch + (self.gamma * (1 - done_batch) * next_values))
 
-            predicted_value1 = self.policy.value_net1(cur_state_batch, cur_feat_batch, actions_batch)
-            predicted_value2 = self.policy.value_net2(cur_state_batch, cur_feat_batch, actions_batch)
+            predicted_value1 = self.policy.value_net1(cur_state_batch, actions_batch)
+            predicted_value2 = self.policy.value_net2(cur_state_batch, actions_batch)
 
             value_loss1 = self.value_criterion1(target_value.detach(), predicted_value1)
             value_loss2 = self.value_criterion2(target_value.detach(), predicted_value2)
@@ -168,12 +166,12 @@ class TD3_BC(Agent):
                     p.requires_grad = False
 
                 # evaluate action taken by policy, in a batch
-                policy_action, _ = self.policy.evaluate_policy_no_noise(cur_state_batch, cur_feat_batch)
+                policy_action, _ = self.policy.evaluate_policy_no_noise(cur_state_batch)
 
                 # take minimum evaluation by critics
                 policy_loss = torch.min(
-                    self.policy.value_net1(cur_state_batch, cur_feat_batch, policy_action), 
-                    self.policy.value_net2(cur_state_batch, cur_feat_batch, policy_action)
+                    self.policy.value_net1(cur_state_batch, policy_action), 
+                    self.policy.value_net2(cur_state_batch, policy_action)
                 )
 
                 # evaluate mean of q values

agents/models/actor_critic_td3_bc.py
@@ -20,25 +20,17 @@ class FeatureExtractor(nn.Module):
         self.n_layers = args.n_rnn_layers
         self.bidirectional = args.bidirectional
         self.directions = args.rnn_directions
-        self.LSTM = nn.LSTM(input_size=2, hidden_size=self.n_hidden, num_layers=self.n_layers, batch_first=True, bidirectional=self.bidirectional)  # (seq_len, batch, input_size)
+        self.LSTM = nn.LSTM(input_size=self.n_features, hidden_size=self.n_hidden, num_layers=self.n_layers, batch_first=True, bidirectional=self.bidirectional)  # (seq_len, batch, input_size)
 
     def forward(self, s, mode):
         if mode == 'batch':
             output, (hid, cell) = self.LSTM(s)
             lstm_output = hid.view(hid.size(1), -1)  # => batch , layers * hid
-            feat = feat.squeeze(1)
         else:
             s = s.unsqueeze(0)  # add batch dimension
             output, (hid, cell) = self.LSTM(s)  # hid = layers * dir, batch, hidden
             lstm_output = hid.squeeze(1)  # remove batch dimension
             lstm_output = torch.flatten(lstm_output)  # hid = layers * hidden_size
-
-        # if self.use_handcraft == 1:  # concat_features = torch.cat((lstm_output, feat), dim=1)
-        #     if mode == 'batch':
-        #         extract_states = torch.cat((lstm_output), dim=1)  # ==>torch.size[256 + 5]
-        #     else:
-        #         extract_states = torch.cat((lstm_output), dim=0)
-        # else:
         
         extract_states = lstm_output
         
@@ -236,8 +228,8 @@ class ValueNetwork(nn.Module):
         self.FeatureExtractor = FeatureExtractor(args)
         self.ValueModule = ValueModule(args, device)
 
-    def forward(self, s, feat, mode='batch'):
-        extract_states, lstmOut = self.FeatureExtractor.forward(s, feat, mode)
+    def forward(self, s, mode='batch'):
+        extract_states, lstmOut = self.FeatureExtractor.forward(s, mode)
         value = self.ValueModule.forward(extract_states)
         return value
 
@@ -248,8 +240,8 @@ class QNetwork(nn.Module):
         self.FeatureExtractor = FeatureExtractor(args)
         self.QvalModel = QvalModel(args, device)
 
-    def forward(self, s, feat, action, mode='batch'):
-        extract_states, lstmOut = self.FeatureExtractor.forward(s, feat, mode)
+    def forward(self, s, action, mode='batch'):
+        extract_states, lstmOut = self.FeatureExtractor.forward(s, mode)
         qvalue = self.QvalModel.forward(extract_states, action, mode)
         return qvalue
 
@@ -291,22 +283,21 @@ class ActorCritic(nn.Module):
             state_value = mu.detach().cpu().numpy()
         )
 
-    def get_action_no_noise(self, s, feat, mode='forward', worker_mode='training'):
+    def get_action_no_noise(self, s, mode='forward', worker_mode='training'):
         s = torch.as_tensor(s, dtype=torch.float32, device=self.device)
-        feat = torch.as_tensor(feat, dtype=torch.float32, device=self.device)
-        mu, sigma, action, log_prob = self.policy_net.forward(s, feat, mode=mode, worker_mode='no noise')
+        mu, sigma, action, log_prob = self.policy_net.forward(s, mode=mode, worker_mode='no noise')
         return action.detach().cpu().numpy(), mu.detach().cpu().numpy(), sigma.detach().cpu().numpy()
 
-    def evaluate_policy(self, state, feat):  # evaluate batch
-        mu, sigma, action, log_prob = self.policy_net.forward(state, feat, mode='batch')
+    def evaluate_policy(self, state):  # evaluate batch
+        mu, sigma, action, log_prob = self.policy_net.forward(state, mode='batch')
         return action, log_prob
 
-    def evaluate_policy_no_noise(self, state, feat):  # evaluate batch
-        mu, sigma, action, log_prob = self.policy_net.forward(state, feat, mode='batch', worker_mode='no noise')
+    def evaluate_policy_no_noise(self, state):  # evaluate batch
+        mu, sigma, action, log_prob = self.policy_net.forward(state, mode='batch', worker_mode='no noise')
         return action, log_prob
 
-    def evaluate_target_policy_noise(self, state, feat):  # evaluate batch
-        mu, sigma, action, log_prob = self.policy_net_target.forward(state, feat, mode='batch', worker_mode='target')
+    def evaluate_target_policy_noise(self, state):  # evaluate batch
+        mu, sigma, action, log_prob = self.policy_net_target.forward(state, mode='batch', worker_mode='target')
         return action, log_prob
 
     def save(self, episode):

utils/core.py
@@ -1,6 +1,7 @@
 import scipy.signal
 import numpy as np
 import torch
+import math
 
 
 # from environment.utils import risk_index
@@ -102,7 +103,7 @@ def calculate_features(data_row, args, env_args):
     info = dict()
 
     info["insulin"] = linear_scaling(x=ins, x_min=args.insulin_min, x_max=args.insulin_max)
-    info["glucose"] = linear_scaling(x=cgm, x_min=args.glucose_min, x_max=args.glucose_max)
+    info["cgm"] = linear_scaling(x=cgm, x_min=args.glucose_min, x_max=args.glucose_max)
     
     info['future_carb'] = 0 #FIXME implement
     info['remaining_time'] = 0 #FIXME implement
@@ -111,3 +112,46 @@ def calculate_features(data_row, args, env_args):
     info['meal_type'] = 0 #FIXME implement
 
     return [ info[feat] for feat in env_args.obs_features]
+
+
+def pump_to_rl_action(pump_action, args, env_args):
+    control_space_type = args.control_space_type
+    pump_max = args.insulin_max
+
+    if control_space_type == 'normal':
+        # pump_action = ((rl_action + 1) / 2) * pump_max
+        rl_action = (2 * pump_action / pump_max) - 1
+
+
+    elif control_space_type == 'sparse':
+        # if agent_action <= 0: agent_action = 0
+        # else: agent_action = agent_action * pump_max
+        #not reversible
+
+        raise NotImplementedError
+
+    elif control_space_type == 'exponential':
+        # pump_action = pump_max * (math.exp((rl_action - 1) * 4))
+        rl_action = math.log((pump_action / pump_max) / 4) + 1
+
+
+
+    elif control_space_type == 'quadratic':
+        # if agent_action < 0:
+        #     agent_action = (agent_action**2) * 0.05
+        #     agent_action = min(0.05, agent_action)
+        # elif agent_action == 0: agent_action = 0
+        # else: agent_action = (agent_action**2) * pump_max
+
+        raise NotImplementedError
+
+    elif control_space_type == 'proportional_quadratic':
+        # if agent_action <= 0.5:
+        #     agent_action = ((agent_action-0.5)**2) * (0.5/(1.5**2))
+        #     agent_action = min(0.5, agent_action)
+        # else:
+        #     agent_action = ((agent_action-0.5)**2) * (pump_max/(0.5**2))
+
+        raise NotImplementedError
+
+    return rl_action

utils/sim_data.py
@@ -11,7 +11,7 @@ from random import randrange
 from decouple import config
 from collections import namedtuple, deque
 from environment.reward_func import composite_reward
-from utils.core import linear_scaling, calculate_features
+from utils.core import linear_scaling, calculate_features, pump_to_rl_action
 MAIN_PATH = config('MAIN_PATH')
 sys.path.insert(1, MAIN_PATH)
 
@@ -365,22 +365,27 @@ def patient_id_to_label(patient_id):
 
 
 
-def convert_trial_into_transitions(data_obj, args, env_args, reward_func=(lambda s : composite_reward(None, s[-1][0]))):
+def convert_trial_into_transitions(data_obj, args, env_args, reward_func=(lambda cgm : composite_reward(None, cgm))):
     #data_obj is a 2D numpy array , rows x columns. Columns are :  cgm, meal, ins, t, meta_data
     #FIXME make features scale to args
     window_size = args.obs_window
 
     rows, _ = data_obj.shape
 
-    states = np.array([calculate_features(data_row for data_row in data_obj)])
+    states = np.array([calculate_features(data_row, args, env_args) for data_row in data_obj])
+
+    actions = [pump_to_rl_action(ins, args, env_args) for ins in data_obj[:, 2]]
+
+    rewards = [linear_scaling(cgm, args.glucose_min, args.glucose_max) for cgm in data_obj[:, 0]]
+
 
     transitions = []
-    for row_n in range(rows-window_size-1):
-        state = states[row_n]
-        action = [states[row_n][-1][1]]
-        reward = [reward_func(state)]
-        next_state = [calculate_features(data_obj[row_n + window_size + 1], args, env_args)]
-        done = [int(row_n == rows - 2)]
+    for row_n in range(window_size, rows-1):
+        state = np.array(states[row_n-window_size: row_n])
+        action = np.array([actions[row_n]])
+        reward = np.array([rewards[row_n]])
+        next_state = np.array(states[row_n-window_size+1: row_n+1])
+        done = np.array([int(row_n == rows - 2)])
         transitions.append(Transition(state, action, reward, next_state, done))
 
     return transitions