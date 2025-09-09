f4207712576f67d7908b4b418f1ee3a8b9d3ec60
Samuel Price <u7482502@anu.edu.au>
2025-09-09 22:44:28 +1000

    improved data importing

--- utils/cln_data.py
+++ utils/cln_data.py
@@ -389,7 +389,8 @@ def convert_df_to_arr(df):
     
     # return arr
 
-    return df[COL_ORDERING].values.tolist()
+    # return df[COL_ORDERING].values.tolist()
+    return df[COL_ORDERING].to_numpy(copy=True)
 
 
 
