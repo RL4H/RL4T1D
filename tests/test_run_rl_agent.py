9cb370f6092267e337184fc2bc6a602b1bfac343
chirathyh <chirath.hettiarachchi@anu.edu.au>
2025-11-14 15:24:38 +1100

    adding tests via github actions

--- tests/test_run_rl_agent.py
+++ tests/test_run_rl_agent.py
@@ -2,7 +2,22 @@ import subprocess
 import pytest
 import os
 
-@pytest.mark.order(2)
+def test_create_env_file():
+    """Test the creation of the .env file with MAIN_PATH."""
+    try:
+        # Run the command to create the .env file
+        subprocess.run("MAIN_PATH=$(pwd) > .env", shell=True, check=True)
+        subprocess.run("SIM_DATA_PATH=$(pwd) > .env", shell=True, check=True)
+
+        # Validate the .env file content
+        # with open(".env", "r") as env_file:
+        #     content = env_file.read()
+        #     assert content.strip() == f"MAIN_PATH={os.getcwd()}", \
+        #         f"Unexpected .env content: {content}"
+    except subprocess.CalledProcessError as e:
+        pytest.fail(f"Failed to create .env file: {e.stderr}")
+
+
 def test_run_rl_agent():
     """Test the RL agent script with PPO algorithm."""
     command = [
@@ -24,18 +39,4 @@ def test_run_rl_agent():
     except subprocess.CalledProcessError as e:
         pytest.fail(f"Command failed with error: {e.stderr}")
 
-@pytest.mark.order(1)
-def test_create_env_file():
-    """Test the creation of the .env file with MAIN_PATH."""
-    try:
-        # Run the command to create the .env file
-        subprocess.run("MAIN_PATH=$(pwd) > .env", shell=True, check=True)
-        subprocess.run("SIM_DATA_PATH=$(pwd) > .env", shell=True, check=True)
 
-        # Validate the .env file content
-        # with open(".env", "r") as env_file:
-        #     content = env_file.read()
-        #     assert content.strip() == f"MAIN_PATH={os.getcwd()}", \
-        #         f"Unexpected .env content: {content}"
-    except subprocess.CalledProcessError as e:
-        pytest.fail(f"Failed to create .env file: {e.stderr}")
